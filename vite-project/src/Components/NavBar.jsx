2ef5afef141a36331c7ef2b0d780e8845547afa5
Purple-syrup <godhanded0@gmail.com>
2023-01-06 20:04:18 +0000

    first react poject

---
+++ vite-project/src/Components/NavBar.jsx
@@ -0,0 +1,13 @@
+
+import reactLogo from "../assets/react.svg"
+export default function NavBar(){
+    return(
+        <div className="header">
+            <img src={reactLogo}/>
+            <h3 className="react-facts">ReactFacts</h3>
+            <h4 className="head-text">React Course - Project 1</h4>
+        </div>
+        
+    )
+}
+
